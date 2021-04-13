b99745a5f57bd88593ffa77b769406c50d5da4b9
Kenkentake <keeeentake0908@gmail.com>
2021-04-13 06:22:35 +0000

    Add data augmentations

--- data/dataset/get_dataset.py
+++ data/dataset/get_dataset.py
@@ -7,13 +7,18 @@ def get_transform_from_list(transform_list: list):
     sequence = []
 
     for t in transform_list:
+        if t == 'random_rotation':
+            sequence.append(transforms.RandomRotation(degrees=15))
+        if t == 'random_horizontal_flip':
+            sequence.append(transforms.RandomHorizontalFlip(p=0.5))
+        if t == 'random_vertical_flip':
+            sequence.append(transforms.RandomVerticalFlip(p=0.5))
+        if t == 'color_jitter':
+            sequence.append(transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1))
         if t == 'to_tensor':
             sequence.append(transforms.ToTensor())
-
         if t == 'normalize':
-            sequence.append(
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-            )
+            sequence.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
 
     return transforms.Compose(sequence)
 
